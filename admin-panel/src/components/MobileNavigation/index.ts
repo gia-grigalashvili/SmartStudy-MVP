f7b7f49db233c238c47eff11875d928b1fce2875
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-18 23:13:34 +0400

    feat(cms): Add mobile navigation component

---
+++ admin-panel/src/components/MobileNavigation/index.ts
@@ -0,0 +1,6 @@
+export * from "./MobileNavigation";
+export * from "./NavigationPreferences";
+export * from "./MobileNavigationMenu";
+export * from "./MobileNavigationFooter";
+export * from "./MobileNavigationHeader";
+export * from "./MobileNavigationBar";
