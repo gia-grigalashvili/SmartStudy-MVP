16b068ab75465a7f907889d6fac6b5fa1c08b579
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-19 09:17:52 +0400

    feat(backend): Add group controller for student

---
+++ backend/src/controllers/student/group.ts
@@ -0,0 +1,149 @@
+import { prisma } from "@/config";
+import { NextFunction, Response, Request } from "express";
+import {
+  sendError,
+  generateWhereInput,
+  getPaginationAndFilters,
+} from "@/utils";
+import { Prisma } from "@prisma/client";
+
+export const fetchGroups = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const studentId = req.user.id;
+
+    const { skip, take, orderBy, search } = getPaginationAndFilters(req);
+
+    const where = generateWhereInput<Prisma.GroupWhereInput>(
+      search,
+      {
+        code: "insensitive",
+        semester: "insensitive",
+      },
+      {
+        enrollments: {
+          some: {
+            studentId,
+          },
+        },
+      }
+    );
+
+    const [groups, count] = await Promise.all([
+      prisma.group.findMany({
+        where,
+        skip,
+        take,
+        orderBy,
+        include: {
+          subjects: {
+            select: {
+              id: true,
+              subject: {
+                select: {
+                  translations: {
+                    include: {
+                      language: {
+                        select: {
+                          code: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          teacher: {
+            select: {
+              id: true,
+              translations: {
+                include: {
+                  language: {
+                    select: {
+                      code: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          academicCalendar: true,
+        },
+      }),
+      prisma.group.count({ where }),
+    ]);
+
+    return res.status(200).json({ data: groups, count });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const fetchGroup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const studentId = req.user.id;
+
+    const group = await prisma.group.findUnique({
+      where: {
+        id,
+        enrollments: {
+          some: {
+            studentId,
+          },
+        },
+      },
+      include: {
+        subjects: {
+          select: {
+            id: true,
+            subject: {
+              select: {
+                translations: {
+                  include: {
+                    language: {
+                      select: {
+                        code: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        teacher: {
+          select: {
+            id: true,
+            translations: {
+              include: {
+                language: {
+                  select: {
+                    code: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        academicCalendar: true,
+      },
+    });
+
+    if (!group) {
+      return sendError(req, res, 404, "groupNotFound");
+    }
+
+    return res.status(200).json({ data: group });
+  } catch (error) {
+    next(error);
+  }
+};
