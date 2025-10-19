bf4f154a7ea49f8635f988833cd7fcbfd3cf0220
gia-grigalashvili <grigalagia07@gmail.com>
2025-10-19 08:01:21 +0400

    ...

---
+++ admin-panel/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Dashboard() {
+  
+
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) {
+  //   console.error("Dashboard fetch error:", error);
+  //   return <div>Error loading dashboard</div>;
+  // }
+
+  // console.log("Dashboard data:", data); // 👈 აქ უკვე დაინახავ პასუხს console-ში
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    </div>
+  );
+}
