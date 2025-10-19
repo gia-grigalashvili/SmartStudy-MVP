a124375f85e0278d2048c4b8f53a26276544b630
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2025-10-19 07:10:12 +0400

    feat(backend): Add subject validations for admin

--- backend/src/validations/admin/index.ts
+++ backend/src/validations/admin/index.ts
@@ -2,3 +2,4 @@ export * from "./auth.validations";
 export * from "./teacher.validations";
 export * from "./student.validations";
 export * from "./group.validations";
+export * from "./subject.validations";
